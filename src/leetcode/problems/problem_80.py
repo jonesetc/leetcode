3ac853b0cd6ec5834bebe0c1692d92aa893360db
kyle jones <2461104+jonesetc@users.noreply.github.com>
2024-09-21 17:34:44 -0700

    double dupe

---
+++ src/leetcode/problems/problem_80.py
@@ -0,0 +1,39 @@
+import click
+
+from ..judge import beginning_list_judge
+from ..test_cases import filter_and_zip
+
+
+TEST_CASES = [
+    [[1, 1, 1, 2, 2, 3]],
+    [[0, 0, 1, 1, 1, 1, 2, 3, 3]],
+]
+
+EXPECTED = [
+    [1, 1, 2, 2, 3],
+    [0, 0, 1, 1, 2, 3, 3],
+]
+
+
+@click.command("80")
+@click.argument("test-cases", nargs=-1, type=click.IntRange(0, len(TEST_CASES) - 1))
+def problem(test_cases: list[int]):
+    for case, expected in filter_and_zip(TEST_CASES, EXPECTED, test_cases):
+        length = solution(*case)
+        actual = case[0]
+        correct = beginning_list_judge(case[0], expected, length)
+        print(f"{case=} {correct=} {expected=} {actual=} {length=}")
+
+
+def solution(nums: list[int]) -> int:
+    if len(nums) < 3:
+        return len(nums)
+
+    last_dupe_index = 2
+
+    for element in nums[2:]:
+        if element != nums[last_dupe_index - 2]:
+            nums[last_dupe_index] = element
+            last_dupe_index += 1
+
+    return last_dupe_index
